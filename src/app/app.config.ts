2dfa6609d2202eaddceb60ebad86b186627b23e3
manojbartakke <148266067+manojbartakke@users.noreply.github.com>
2026-08-27 16:13:30 +0530

    fix: remove duplicate provideHttpClient import after merge

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import {
 } from '@angular/core';
 import {provideHttpClient, withFetch} from '@angular/common/http';
 import {provideRouter} from '@angular/router';
-import {provideHttpClient, withFetch} from '@angular/common/http';
 
 import {routes} from './app.routes';
 
